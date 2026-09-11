c78a8dc365f6c355e9be6732302e319401f9c47e
Abdulrahman <Abdulrahman-Mostafa53>
2026-09-11 09:50:36 +0300

    add camera node to subscribe on /mono/image

--- robot_navigator/setup.py
+++ robot_navigator/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch',['launch/go_bot.launch.py'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            "yolo_camera = robot_navigator.yolo_camera:main"
         ],
     },
 )
